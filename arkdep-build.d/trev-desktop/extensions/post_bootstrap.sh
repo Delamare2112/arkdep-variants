bfa3b30a2c1d35c4a8016cb71e14b667042dbdd9
Trevor Berninger <baladashman@protonmail.com>
2026-05-03 12:29:25 -0700

    ensure nvidia GSP firmware is within initramfs

--- arkdep-build.d/trev-desktop/extensions/post_bootstrap.sh
+++ arkdep-build.d/trev-desktop/extensions/post_bootstrap.sh
@@ -4,3 +4,8 @@ set -e
 set -x
 
 sed -i 's/install --no-depmod/install --verbose --no-depmod/g' "${workdir}/usr/share/libalpm/scripts/dkms"
+
+cd ${workdir}
+for GSP in $(find "usr/lib/firmware/nvidia" -type f -name 'gsp_*.bin'); do
+    echo "install_items+=\" /${GSP}" >> etc/dracut.conf.d/ensure-gsp.conf
+done
